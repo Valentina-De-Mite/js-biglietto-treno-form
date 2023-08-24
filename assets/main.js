df799df48ef377e396d540f4acb803f8c2ce0a74
Valentina De Mite <valesakura97@gmail.com>
2023-08-24 16:43:40 +0200

    second commit

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,52 @@ Solo una volta che il milestone 1 sarà completo e funzionante allora realizzere
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.
 */
+
+/* Strumenti:
+- prompt x2 (km del viaggio e eta' del passeggero)
+- console.log(typeof Number(quantity))
+- moltiplico i km per 0.21
+- if eta' > o uguale 65: -40%
+- if eta' < o = 17: -20%
+- console.log 
+*/
+
+// chiedo i km del viaggio
+
+const userName = prompt("Inserisci qui il tuo nome");
+
+const trainJourney = Number(prompt("Inserisci i km del tuo percorso:"));
+
+const userAge = Number(prompt("Inserisci la tua eta':"));
+
+const price_per_km = 0.21;
+
+console.log(Number(userAge));
+
+console.log(Number(trainJourney));
+
+// calcolo il prezzo del biglietto
+let ticket_price = trainJourney * price_per_km;
+
+let discount = 0;
+// calcolo lo sconto minorenni
+
+if (userAge < 18) {
+  console.log("sei minorenne, ti faccio il 20% di sconto");
+  discount = ticket_price * 0.2;
+  ticket_price = ticket_price - discount;
+}
+
+// calcolo lo sconto > 65
+
+if (userAge > 65) {
+  console.log("sei over 65, ti faccio il 40% di sconto");
+  discount = ticket_price * 0.4;
+  ticket_price = ticket_price - discount;
+}
+
+console.log("il prezzo del biglietto = " + ticket_price);
+
+// stampo in pagina
+
+ticket_price.innerHTML = +"Euro";
